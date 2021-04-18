0cad63c261d85d97ad5ae991b313c441a4916496
pani-marinka <pani_marinka@ukr.net>
2021-04-18 23:02:34 +0300

    DEV-009_3

--- src/main/java/service/UsWaitBattService.java
+++ src/main/java/service/UsWaitBattService.java
@@ -3,11 +3,11 @@ package service;
 import data.Battle;
 import data.User;
 import org.springframework.stereotype.Service;
+
 import java.util.*;
 
 @Service
 public class UsWaitBattService {
-
 
 
     //private static Set<User> usWaitBattle = new LinkedHashSet<>();
@@ -29,7 +29,7 @@ public class UsWaitBattService {
 
         for (String id : keys) {
             user = waitBattle.get(id);
-             return user;
+            return user;
         }
         return null;
     }
@@ -44,18 +44,21 @@ public class UsWaitBattService {
     }
 
 
-
     public void removeWaitBattle(String id) {
         waitBattle.remove(id);
     }
 
 
-    public String addPrepareForBattle( User user1, User user2) {
+    public String addPrepareForBattle(User user1, User user2) {
         String idBattle = UUID.randomUUID().toString();
-        Battle battle1 = new Battle(idBattle, user1.getId(),user1.getLogin(), user1.getName(), user1.getRaceid(), 0, user1.getPoints(), user1.getMoney(), user1.getGold(), null, user1.getDeck(), null);
-         usBattle.add(battle1);
-        Battle battle2 = new Battle(idBattle, user2.getId(),user2.getLogin(), user2.getName(), user2.getRaceid(), 0, user2.getPoints(), user2.getMoney(), user2.getGold(), null, user2.getDeck(), null);
-        usBattle.add(battle2);
+        Battle battle = new Battle(idBattle,
+                user1.getId(), user1.getLogin(), user1.getName(), user1.getRaceid(),
+                0, 0, 30, 30, 2,
+                user1.getPoints(), user1.getMoney(), user1.getGold(), null, user1.getDeck(), null,
+                user2.getId(), user2.getLogin(), user2.getName(), user2.getRaceid(),
+                0, 0, 30, 30, 2,
+                user2.getPoints(), user2.getMoney(), user2.getGold(), null, user2.getDeck(), null);
+        usBattle.add(battle);
         removeWaitBattle(user1.getId());
         removeWaitBattle(user2.getId());
         return idBattle;
