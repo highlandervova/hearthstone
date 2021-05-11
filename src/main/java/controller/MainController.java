e01e2045263d43b6062116d570a058555905771d
pani_marinka@ukr.net <wolf3812>
2021-05-11 13:18:38 +0300

    add change deckUs

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,6 +1,7 @@
 package controller;
 
 
+import data.Card;
 import data.User;
 import enums.RedirectPath;
 import enums.RequestParameter;
@@ -67,8 +68,11 @@ public class MainController  implements HttpSessionListener
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView mainGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         ModelAndView out = new ModelAndView("main");
-        userDeckService.createUsMainDeck();
-        userDeckService.clearUsOwnDeck();
+//        userDeckService.createUsMainDeck();
+//        Set<Card> usOwnDeck = new LinkedHashSet<>();
+//        Set<Card> userDeck = new LinkedHashSet<>();
+//        Set<Card> usMainDeck = new LinkedHashSet<>();
+        //userDeckService.clearUsOwnDeck();
 
         out.addObject("title", "HearthStone");
         out.addObject("pathEditUser", RedirectPath.EDIT_USER.getValue());
@@ -102,6 +106,7 @@ public class MainController  implements HttpSessionListener
         if (req.getParameter(RequestParameter.LOGOFF.getValue()) != null) {
             User user = (User) req.getSession().getAttribute(AUTHENTICATED.getValue());
             userOnlineService.remove(user);
+            userDeckService.removeListDeck(user.getId());
             req.getSession().invalidate();
             int i = userOnlineService.getUsOnlineCount();
             out.addObject("Login", "");
@@ -110,16 +115,16 @@ public class MainController  implements HttpSessionListener
 
         if (req.getParameter(RequestParameter.DECK.getValue()) != null) {
 
-            userDeckService.createUsMainDeck();
-            userDeckService.clearUsOwnDeck();
-
-            User user = (User) req.getSession().getAttribute(AUTHENTICATED.getValue());
-            if (user.getDeck() !=null ) {
-                userDeckService.getUsDeck(user);
-                if (userDeckService.getUserDeckCount() > 0) {
-                    userDeckService.createUsOwnDeckFromUserDeck();
-                }
-            }
+//            userDeckService.createUsMainDeck();
+//        //    userDeckService.clearUsOwnDeck();
+//
+//            User user = (User) req.getSession().getAttribute(AUTHENTICATED.getValue());
+//            if (user.getDeck() !=null ) {
+//                userDeckService.getUsDeck(user, userDeck);
+//                if (userDeckService.getUserDeckCount(userDeck) > 0) {
+//                    userDeckService.createUsOwnDeckFromUserDeck(userDeck, usOwnDeck, usMainDeck);
+//                }
+//            }
             resp.sendRedirect("deckUs?id=all");
         }
 
--- src/main/java/service/UserDeckService.java
+++ src/main/java/service/UserDeckService.java
@@ -27,94 +27,125 @@ public class UserDeckService {
     }
 
 
-    private final Set<Card> usMainDeck = new LinkedHashSet<>();
-    private Set<Card> usOwnDeck = new LinkedHashSet<>(); //idUser ???
-    private Set<Card> userDeck = new LinkedHashSet<>();  // from user.getDeck
+    private Map<String, ArrayList> mainDeck = new HashMap<>();   // Set<Card> usMainDeck;// = new LinkedHashSet<>();
+    private Map<String, ArrayList> ownDeck = new HashMap<>();//idUser ???
+    private Map<String, ArrayList> userDeck = new HashMap<>();  // from user.getDeck
 
-    public int getUsMainDeckCount() {
-        return usMainDeck.size();
-    }
 
-    public int getUsOwnDeckCount() {
-        return usOwnDeck.size();
-    }
+    public int getUsOwnDeckCount(String userId) {
 
-    public int getUserDeckCount() {
-        return userDeck.size();
-    }
+        ArrayList usOwnDeck = new ArrayList();
+        Set <String> keys = ownDeck.keySet();
 
-    public Set<Card> createUsMainDeck() {
+        for (String id : keys) {
+            if (id == userId)
+                usOwnDeck = ownDeck.get(id);
+
+        }
+       return usOwnDeck.size();
+          }
+
+
+    public void createUsMainDeck(String userId) {
         Collection<Card> out = cardService.get();
+        ArrayList in = new ArrayList();
         for (Card card : out) {
             if (card.getId() != 0 && card.getId() != -1 && card.getId() != -2 && card.getId() != 21) {  // remove from deck shirt &mana& shirtDeck&boar
-                usMainDeck.add(card);
+                in.add(card);
             }
+            mainDeck.put(userId, in);
         }
+
+
+    }
+
+    public ArrayList getUsMainDeck(String userId) {
+        ArrayList usMainDeck = new ArrayList();
+        Set <String> keys = mainDeck.keySet();
+
+        for (String id : keys) {
+            if (id == userId)
+            usMainDeck = mainDeck.get(id);
+
+        }
+
         return usMainDeck;
     }
 
-    public Set<Card> getUsMainDeck() {
-        return usMainDeck;
-    }
 
-    public Set<Card> getUsOwnDeck() {
+
+    public ArrayList getUsOwnDeck(String userId) {
+        ArrayList usOwnDeck = new ArrayList();
+
+        Set <String> keys = ownDeck.keySet();
+
+        for (String id : keys) {
+            if (id == userId)
+                usOwnDeck = ownDeck.get(id);
+
+        }
         return usOwnDeck;
     }
 
-    public Set<Card> getUsDeck(User user) {
-        if (user.getDeck() !=null) {
-            userDeck.clear();
+
+
+    public void createOwnDeckFromUserDeck(User user) {
+        if (user.getDeck() != null) {
+            ArrayList inOwn = new ArrayList();
+            ArrayList inMain = getUsMainDeck(user.getId());
+
             List<Integer> usDeck = usWaitBattService.createDeckHeroBattle(user.getDeck());
             if (usDeck.size() > 0) {
                 Collection<Card> out = cardService.get();
                 for (int i = 0; i < 10; i++) {
                     for (Card card : out) {
-                        if (usDeck.get(i) == card.getId())
-                            userDeck.add(card);
+                        if (usDeck.get(i) == card.getId()) {
+                            inOwn.add(card);
+                            inMain.remove(card);
+                        }
                     }
                 }
+
             }
 
+            ownDeck.put(user.getId(), inOwn);
+            mainDeck.put(user.getId(), inMain);
         }
-            return userDeck;
-
-
     }
 
 
-
-    public void createUsOwnDeckFromUserDeck () {
-       for (Card cardUs:userDeck)
-           usOwnDeck.add(cardUs);
-
-        for (Card cardUs:usOwnDeck){
-            usMainDeck.remove(cardUs);
-
-    }
-    }
-
-    public Set<Card> clearUsOwnDeck() {
-        usOwnDeck.clear();
-        return usOwnDeck;
+    public void removeListDeck(String userId) {
+        ownDeck.remove(userId);
+        mainDeck.remove(userId);
     }
 
 
-    public void addForUsMainDeck(Card card) {
-        usMainDeck.add(card);
-        usOwnDeck.remove(card);
+    public void addForUsMainDeck(Card card, String userId) {
+        ArrayList inOwn = getUsOwnDeck(userId);
+        ArrayList inMain = getUsMainDeck(userId);
+        inOwn.remove(card);
+        inMain.add(card);
+        ownDeck.put(userId, inOwn);
+        mainDeck.put(userId, inMain);
     }
 
-    public void addForUsOwnDeck(Card card) {
-        usOwnDeck.add(card);
-        usMainDeck.remove(card);
+    public void addForUsOwnDeck(Card card, String userId) {
+        ArrayList inOwn = getUsOwnDeck(userId);
+        ArrayList inMain = getUsMainDeck(userId);
+        if (inOwn.size()<10){
+        inOwn.add(card);
+        inMain.remove(card);
+        }
+        ownDeck.put(userId, inOwn);
+        mainDeck.put(userId, inMain);
     }
 
 
     public void setUsOwnDeck(User user) {
         String s = "{'id':'";
         int i = 0;
-        Collection<Card> in = getUsOwnDeck();
-        if (getUsOwnDeckCount() == 10) {
+        Collection<Card> in = getUsOwnDeck(user.getId());
+        if (getUsOwnDeckCount(user.getId()) == 10) {
             for (Card card : in) {
                 if (i < 9) {
                     s = s + card.getId() + ",";
